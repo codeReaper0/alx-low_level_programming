8dd7a82443d90749d37bda9064a38ae4aaec4cc8
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-08 23:30:02 +0100

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-/*
-* main prints "with proper grammar, but the outcome is a piece of art" with a new line
-*
-* returns 0
-*/
-
-int main(void)
+/**
+ *  main - prints with printf
+ *
+ *  Return: Always 0
+ */int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
