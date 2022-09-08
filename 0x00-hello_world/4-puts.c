37ace88658e6b34207d99e7c47aff9be1a29560a
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-08 23:27:31 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-/*
-* main prints "Programming is like building a multilingual puzzle"
-*
-* returns 0
-*/
+/**
+ *  main 
+ *
+ *  Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
