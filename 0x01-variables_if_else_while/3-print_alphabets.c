5f24d83b8381e8fb76087a3f73f51528df6f822d
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-09 20:42:21 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - prints the alphabet in lowercase, and then in uppercase
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	
+	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+	putchar(alphabets);
+
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - prints the alphabet in lowercase, followed by a new line
+ *	Print all the letters except q and e
+ *  Return: Always 0
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'q' && alphabets != 'e') {
+			putchar(alphabets);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
