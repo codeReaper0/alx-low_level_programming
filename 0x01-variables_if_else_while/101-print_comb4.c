8f02463b2e4bf1adcf28fb6dd177f4b277f7d712
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-09 21:47:55 +0100

    three combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,19 +8,23 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int num1, num2, num3;
 
 	for (num1 = 0; num1 < 9; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
-			if (num1 == 8 && num2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
