53b158c4831fee52145209de7a09febc15cf125e
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-09 04:21:47 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- *  main - prints with 
+ *  main - prints with
  *
  *  Return: Always 0
  */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
