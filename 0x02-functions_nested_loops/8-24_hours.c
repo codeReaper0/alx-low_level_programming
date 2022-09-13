01b858daa9a7a4d47b92d368df374eb659184e14
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-13 23:04:00 +0100

    c functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - prints the last digit of a number
+* @n:  number to be checked
+* Return: the value of the last digit
+*/
+
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	_putchar(i);
+	
+}
