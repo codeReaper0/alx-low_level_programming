3e3a45858ea3d4b00547c17c51b5f530c29abd27
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-13 18:04:33 +0100

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
 * _islower -  checks for lowercase character.
-* @c - character has to be checked
+* @c:  character has to be checked
 * Return: 1 if character is lowercase, 0 otherwise.
 */
 
