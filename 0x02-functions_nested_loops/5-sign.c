5efc5e40f18389bfa9b6f939c965a953d872af6b
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-13 23:07:34 +0100

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
 * print_sign -   prints the sign of a number.
-* @c:  character has to be checked
+* @n:  character has to be checked
 * Return: Returns 1 and prints + if n is greater than zero,
 * Returns 0 and prints 0 if n is zero
 * Returns -1 and prints - if n is less than zero
