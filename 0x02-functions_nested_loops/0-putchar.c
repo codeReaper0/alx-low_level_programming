9697c0512656eea88ce266613c04213e59af718a
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-13 16:05:26 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-/* main - print _putchar
-*
+/**
+* main - print _putchar
 * Returns - Always 0
 */
 
-int main(void) {
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
