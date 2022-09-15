5168057d77ee40e04936de28a6d7a431d0eeb772
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-15 11:32:46 +0100

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
 	if (a > b && a > c)
 	{
-		largest = a;
+	largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b > c)
 	{
-		largest = b;
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
-    return (largest);
+	return (largest);
 }
