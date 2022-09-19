46d9a182cd2251d399afee6868bab774cb34c115
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-19 16:58:49 +0100

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,12 @@
 /**
  * _strlen - returns the length of a string
  * @n: checks the value
+ * Tella Boluwatife - Author
  * Return: Always 0.
  */
 
 int _strlen(char *s)
 {
-	_strlen(*s);
+	char *s[50] = "Program";
+	printf("%zu", _strlen(*s));
 }
