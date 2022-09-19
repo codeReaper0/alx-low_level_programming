7efc19422cf3064683d7c6fc0cb81ec6acc3c8c4
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-19 16:51:19 +0100

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int(int *a, int *b) - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: first integer
  * @b: second integer
  * Tella Boluwatife - Author
