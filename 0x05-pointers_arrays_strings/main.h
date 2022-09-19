c1e82f8940da872de469b6242a966595afeeffd6
Tella Boluwatife <boluwatifetella@gmail.com>
2022-09-19 17:25:31 +0100

    Pointers, arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,12 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif 
